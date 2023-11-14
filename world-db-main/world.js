42b30a3b66cfbec4b1e7e470744bca212966c5f3
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2023-11-13 20:16:47 -0400

    added lookup cities button and required codes for functionality

--- world-db-main/world.js
+++ world-db-main/world.js
@@ -1,12 +1,25 @@
 $(document).ready(() => {
     const lookupBtn = $('#lookup');
+    const lookupCitiesBtn = $('#lookupCities');
 
     lookupBtn.click(() => {
         const input = $('#country').val();
-        $.ajax('world.php?country='+input, {
+        $.ajax(`world.php?country=${input}`, {
             method: 'GET',
             data: {
                 country: input
+            }
+        }).done(response => $('#result').html(response))
+            .fail(() => alert('There was a problem with the request.'));
+    });
+
+    lookupCitiesBtn.click(() => {
+        const input = $('#country').val();
+        $.ajax(`world.php?country=${input}&lookup=cities`, {
+            method: 'GET',
+            data: {
+                country: input,
+                lookup: 'cities'
             }
         }).done(response => $('#result').html(response))
             .fail(() => alert('There was a problem with the request.'));
--- world-db-main/world.php
+++ world-db-main/world.php
@@ -11,14 +11,45 @@ try {
 }
 
 $country = $_GET['country'];
-$stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
-$stmt->execute(['country' => "%$country%"]);
+$lookup = $_GET['lookup'] ?? 'countries';
 
+if ($lookup === 'cities') {
+    $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE :country");
+} else {
+    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+}
+
+$stmt->execute(['country' => "%$country%"]);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<ul>
+<table>
+    <tr>
+        <?php if ($lookup === 'cities'): ?>
+            <th>Name</th>
+            <th>District</th>
+            <th>Population</th>
+        <?php else: ?>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        <?php endif; ?>
+    </tr>
     <?php foreach ($results as $row): ?>
-        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+        <tr>
+            <?php if ($lookup === 'cities'): ?>
+                <td><?= $row['name'] ?></td>
+                <td><?= $row['district'] ?></td>
+                <td><?= $row['population'] ?></td>
+            <?php else: ?>
+                <td><?= $row['name'] ?></td>
+                <td><?= $row['continent'] ?></td>
+                <td><?= $row['independence_year'] ?></td>
+                <td><?= $row['head_of_state'] ?></td>
+            <?php endif; ?>
+        </tr>
     <?php endforeach; ?>
-</ul>
+</table>
+
+
 
